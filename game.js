992d543a41f830a6ae36a8ed393298892aa04ed7
carterdeacon <carter.deacon@gmail.com>
2021-12-02 11:36:43 +1000

    initial commit with javascript outline, css and html done

---
+++ game.js
@@ -0,0 +1,14 @@
+// Variables for players
+let player1 = "X";
+let player2 = "O";
+
+// Variables for gameplay
+var allBoxes = document.querySelectorAll('.square');
+var result = document.querySelector('.game-result');
+var turnDisplay = document.querySelector('.turn');
+var resetButton = document.querySelector('#reset-btn');
+
+//function to handle flow of the game, change player turn / playing of squares
+
+// function to handle reset button, reset playercoubtaand restart board
+
